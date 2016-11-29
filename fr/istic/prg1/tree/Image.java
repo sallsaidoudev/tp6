036f207cad8d690358d504ad1fd8c45de1eb0dcc
Berenger NGUYEN NHON <berenger.nguyen_nhon@yahoo.fr>
2016-11-29 13:20:42 +0100

    wip

--- fr/istic/prg1/tree/Image.java
+++ fr/istic/prg1/tree/Image.java
@@ -52,8 +52,26 @@ public class Image extends AbstractImage {
 			e.printStackTrace();
 			System.exit(0);
 		}
+		return false;
 	}
 
+	/**
+	 * Recherche le noeud de l'arbre contenant le point dont les coordonnees sont passees en parametre.
+	 * Apres l'appel a cette fonction, ce noeud est le noeud courant de l'iterateur.
+	 * 
+	 * @param it
+	 * 			iterateur sur l'arbre a parcourir
+	 * @param x
+	 * 			abscisse du point a rechercher
+	 * @param y
+	 * 			ordonnee du point a rechercher
+	 * @param cutType
+	 * 			sens du decoupage au noeud courant de it : 'v' pour vertical ; 'h' pour horizontal
+	 * @param x_size
+	 * 			largeur de la portion de fenetre correspondant au noeud courant de it
+	 * @param y_size
+	 * 			hauteur de la portion de fenetre correspondant au noeud courant de it
+	 */
 	private void explore_search (Iterator<Node> it, int x, int y, char cutType, int x_size, int y_size) {
 		if (it.nodeType() != NodeType.LEAF) {
 			if (cutType == 'v') {
@@ -80,11 +98,54 @@ public class Image extends AbstractImage {
 	 */
 	@Override
 	public void affect(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction � �crire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
+		try {
+			assert (!image2.isEmpty()) : "L'image est vide.";
+			
+			Iterator<Node> it = iterator(),
+					it2 = image2.iterator();
+			
+			// si this n'est pas vide, la vider
+			if (!isEmpty()) it.clear();
+			
+			explore_copy (it, it2, it2.getValue());
+			
+		} catch (AssertionError e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	/**
+	 * Copie les noeuds d'un arbre dans un arbre vide
+	 * 
+	 * @param it
+	 * 			iterateur sur l'arbre vide a remplir
+	 * @param it2
+	 * 			iterateur sur l'arbre a copier
+	 * @param root
+	 * 			noeud racine de l'arbre a copier
+	 */
+	private void explore_copy (Iterator<Node> it, Iterator<Node> it2, Node root) {
+		
+		// copier la valeur du noeud courant
+		it.addValue(Node.valueOf(it2.getValue().state));
+		
+		// si le noeud a des fils, parcour recursif
+		if (it2.nodeType() == NodeType.DOUBLE) {
+			it2.goRight();
+			it.goRight();
+			explore_copy (it, it2, null);
+			it2.goLeft();
+			it.goLeft();
+			explore_copy (it, it2, null);
+		}
+		
+		// si le noeud n'est pas la racine, remonter au noeud parent
+		if (!it2.getValue().equals(root)) {
+			it.goUp();
+			it2.goUp();
+		}
+		
 	}
 
 	/**
@@ -96,11 +157,40 @@ public class Image extends AbstractImage {
 	 */
 	@Override
 	public void rotate180(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction � �crire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
+		try {
+			assert (!image2.isEmpty()) : "L'image est vide.";
+			
+			Iterator<Node> it = iterator(),
+						it2 = image2.iterator();
+			
+			if (!isEmpty()) it.clear();
+			
+			explore_rotate180 (it, it2, it2.getValue());
+			
+		} catch (AssertionError e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	private void explore_rotate180 (Iterator<Node> it, Iterator<Node> it2, Node root) {
+		
+		it.addValue( Node.valueOf( it2.getValue().state));
+		
+		if (it2.nodeType() == NodeType.DOUBLE) {
+			it.goLeft();
+			it2.goRight();
+			explore_rotate180 (it, it2, null);
+			it.goRight();
+			it2.goLeft();
+			explore_rotate180 (it, it2, null);
+		}
+		
+		if (!it2.getValue().equals(root)) {
+			it.goUp();
+			it2.goUp();
+		}
+		
 	}
 
 	/**
@@ -143,6 +233,14 @@ public class Image extends AbstractImage {
 		}
 	}
 	
+	/**
+	 * Parcour l'arbre et inverse la valeur des feuilles
+	 *  
+	 * @param it
+	 * 			iterateur sur l'arbre a parcourir
+	 * @param root
+	 * 			racine de l'arbre
+	 */
 	private void explore_switch (Iterator<Node> it, Node root) {
 		System.out.println (it.getValue().state);
 		if (it.nodeType() == NodeType.LEAF) {
@@ -199,11 +297,32 @@ public class Image extends AbstractImage {
 	 */
 	@Override
 	public void zoomIn(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction � �crire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
+		try {
+			assert (!image2.isEmpty()) : "L'image est vide.";
+			
+			Iterator<Node> it = iterator(),
+					it2 = image2.iterator();
+			
+			if (!isEmpty()) it.clear();
+			
+			// Cas d'une image pleine
+			if (it2.nodeType() == NodeType.LEAF) affect (image2);
+			
+			// Cas d'une image dont la moitie superieure est pleine
+			it2.goLeft();
+			if (it2.nodeType() == NodeType.LEAF) it.addValue( Node.valueOf( it2.getValue().state));
+			else {
+				
+				// Cas general
+				it2.goLeft();
+				explore_copy (it, it2, it2.getValue());
+				
+			}
+			
+		} catch (AssertionError e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
 
 	/**
@@ -216,11 +335,37 @@ public class Image extends AbstractImage {
 	 */
 	@Override
 	public void zoomOut(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction � �crire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
+		try {
+			assert (!image2.isEmpty()) : "L'image est vide.";
+			
+			Iterator<Node> it = iterator(),
+					it2 = image2.iterator();
+			
+			if (!isEmpty()) it.clear();
+			
+			// la racine est un noeud double
+			it.addValue( Node.valueOf(2));
+			
+			// eteindre la moitie inferieure de this
+			it.goRight();
+			it.addValue( Node.valueOf(0));
+			it.goUp();
+			
+			// eteindre le quart superieur droit
+			it.goLeft();
+			it.addValue( Node.valueOf(2));
+			it.goRight();
+			it.addValue( Node.valueOf(0));
+			it.goUp();
+			
+			// copier image2 dans le quart superieur gauche
+			it.goLeft();
+			explore_copy (it, it2, it2.getValue());
+			
+		} catch (AssertionError e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
 
 	/**
@@ -251,11 +396,53 @@ public class Image extends AbstractImage {
 	 */
 	@Override
 	public void union(AbstractImage image1, AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction � �crire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
+		try {
+			assert (!isEmpty()) : "La premiere image est vide.";
+			assert (!image2.isEmpty()) : "La seconde image est vide";
+			
+			Iterator<Node> it = iterator(),
+					it2 = image2.iterator();
+			
+			explore_union (it, it2, it2.getValue());
+			
+		} catch (AssertionError e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	private void explore_union (Iterator<Node> it, Iterator<Node> it2, Node root) {
+		
+		if (it.nodeType() == NodeType.LEAF) {
+			if (it.getValue().state == 0)
+				explore_copy (it, it2, it2.getValue());
+		
+		} else {
+			
+			if (it2.nodeType() == NodeType.LEAF) {
+				if (it2.getValue().state == 1) {
+					it.clear();
+					it.addValue( Node.valueOf(1));
+				}
+			} else {
+				
+				it.goRight();
+				it2.goRight();
+				explore_union (it, it2, null);
+				
+				it.goLeft();
+				it2.goLeft();
+				explore_union (it, it2, null);
+				
+			}
+			
+		}
+		
+		if (!it2.getValue().equals(root)) {
+			it.goUp();
+			it2.goUp();
+		}
+	
 	}
 
 	/**
@@ -266,12 +453,77 @@ public class Image extends AbstractImage {
 	 */
 	@Override
 	public boolean testDiagonal() {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction � �crire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-	    return false;
+		boolean ret = false;
+		
+		if (!isEmpty()) {
+			
+			Iterator<Node> it = iterator();
+			
+			// cas d'une image pleine
+			if (it.nodeType() == NodeType.LEAF)
+				return it.getValue().state == 1;
+			
+			ret = explore_diagonal (it);
+			
+		}
+	    return ret;
+	}
+	
+	/**
+	 * Teste si les pixels de la diagonale de la section d'image correspondant
+	 * au noeud courant de it sont allumes.
+	 * 
+	 * @param it
+	 * 			iterateur sur l'arbre a tester
+	 * @return
+	 * 			true si les pixels sont allumes,
+	 * 			false sinon.
+	 */
+	private boolean explore_diagonal (Iterator<Node> it) {
+		
+		if (it.nodeType() == NodeType.LEAF)
+			return it.getValue().state == 1;
+		
+		boolean ret;
+		
+		// tester la moitie superieure
+		it.goLeft();
+		
+		// si la section est pleine
+		if (it.nodeType() == NodeType.LEAF) {
+			ret = it.getValue().state == 1;
+			it.goUp();
+			
+		// tester le quart superieur gauche sinon
+		} else {
+			
+			it.goLeft();
+			ret = explore_diagonal (it);
+			it.goUp();
+			it.goUp();
+			
+		}
+		
+		// tester la moitie inferieure
+		it.goRight();
+		
+		// si la section est pleine
+		if (it.nodeType() == NodeType.LEAF) {
+			ret = ret && it.getValue().state == 1;
+			it.goUp();
+		}
+		
+		// tester le quart inferieur droit sinon
+		else {
+			
+			it.goRight();
+			ret = ret && explore_diagonal (it);
+			it.goUp();
+			it.goUp();
+			
+		}
+		
+		return ret;
 	}
 
 	/**
@@ -306,12 +558,53 @@ public class Image extends AbstractImage {
 	 */
 	@Override
 	public boolean isIncludedIn(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction � �crire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-	    return false;
+		try {
+			assert (!isEmpty()) : "La premiere image est vide.";
+			assert (!image2.isEmpty()) : "La seconde image est vide.";
+			
+			Iterator<Node> it = iterator(),
+					it2 = image2.iterator();
+			
+			return explore_inclusion (it, it2, it2.getValue());
+			
+		} catch (AssertionError e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		return false;
+	}
+	
+	private boolean explore_inclusion (Iterator<Node> it, Iterator<Node> it2, Node root) {
+		boolean ret = false, keepGoing = true;
+		
+		// une section vide est toujours incluse
+		if (it.nodeType() == NodeType.LEAF) {
+			ret = it.getValue().state == 0;
+			keepGoing = false;
+		}
+		
+		// si la section de image2 est pleine, elle ne peut contenir que si elle est allumee,
+		// a moins que la section de this soit aussi vide
+		if (it2.nodeType() == NodeType.LEAF) {
+			ret = ret || it2.getValue().state == 1;
+			keepGoing = false;
+		}
+		
+		if (keepGoing) {
+			it.goRight();
+			it2.goRight();
+			ret = explore_inclusion (it, it2, null);
+			it.goLeft();
+			it2.goRight();
+			ret = ret && explore_inclusion (it, it2, null);
+		}
+		
+		if (!it2.getValue().equals(root)) {
+			it.goUp();
+			it2.goUp();
+		}
+		
+		return ret;
 	}
 
 }
